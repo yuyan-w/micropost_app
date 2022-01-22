bd760952feb85935d70a566a4ccc06c677b1759d
yuyan <yuyan0904@gmail.com>
2022-01-22 12:58:34 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,20 @@ RSpec.describe "StaticPages", type: :request do
   end
 
   describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
+    # it "returns http success" do
+    #   get "/static_pages/help"
+    #   expect(response).to have_http_status(:success)
+    # end
+    it "正常レスポンスコードが返ってくる" do
+      get static_pages_help_url
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Get /about" do
+    it "正常レスポンスコードが返ってくる" do
+      get static_pages_about_url
+      expect(response.status).to eq 200
     end
   end
 
